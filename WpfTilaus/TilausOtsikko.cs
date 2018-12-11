d62834feaf3af806858d84259d656a4f650b9321
Jasmin <jasmin.kaalinpaa@gmail.com>
2018-12-11 15:24:02 +0200

    Add project files.

---
+++ WpfTilaus/TilausOtsikko.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTilaus
+{
+    class TilausOtsikko
+    {
+        private DateTime tilausPvm;
+        private DateTime toimitusPvm;
+        public int TilausNumero { get; set; }
+        public string AsiakkaanNimi { get; set; }
+        public int AsiakasNumero { get; set; }
+        public string ToimitusOsoite { get; set; }
+        public DateTime TilausPvm 
+        {
+            get
+            {
+                return tilausPvm;
+            }
+            set
+            {
+                if (value <= DateTime.Today)
+                {
+                    tilausPvm = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Tilauspäivämäärä ei voi olla tulevaisuudessa!");
+                }
+            }
+        }
+        public DateTime ToimitusPvm {
+      
+            get
+            {
+                return toimitusPvm;
+            }
+            set
+            {
+                if (value > DateTime.Today)
+                {
+                    toimitusPvm = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("");
+                }
+            }
+        }
+    }
+}
